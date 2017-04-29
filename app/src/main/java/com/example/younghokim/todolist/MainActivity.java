be83199a313eddc5d143cc1d95ff16a23703aa51
donnaknew <donnaknew@gmail.com>
2017-04-29 16:33:33 +0900

    TODOLIST의 전체적인 틀, 디자인 완성

---
+++ app/src/main/java/com/example/younghokim/todolist/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.younghokim.todolist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.yalantis.beamazingtoday.interfaces.AnimationType;
+import com.yalantis.beamazingtoday.interfaces.BatModel;
+import com.yalantis.beamazingtoday.listeners.BatListener;
+import com.yalantis.beamazingtoday.listeners.OnItemClickListener;
+import com.yalantis.beamazingtoday.listeners.OnOutsideClickedListener;
+import com.yalantis.beamazingtoday.ui.adapter.BatAdapter;
+import com.yalantis.beamazingtoday.ui.animator.BatItemAnimator;
+import com.yalantis.beamazingtoday.ui.callback.BatCallback;
+import com.yalantis.beamazingtoday.ui.widget.BatRecyclerView;
+import com.yalantis.beamazingtoday.util.TypefaceUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements BatListener, OnItemClickListener, OnOutsideClickedListener {
+
+    private BatRecyclerView mRecyclerView;
+    private BatAdapter mAdapter;
+    private List<BatModel> mGoals;
+    private BatItemAnimator mAnimator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        ((TextView) findViewById(R.id.text_title)).setTypeface(TypefaceUtil.getAvenirTypeface(this));
+
+        mRecyclerView = (BatRecyclerView) findViewById(R.id.bat_recycler_view);
+        mAnimator = new BatItemAnimator();
+
+        mRecyclerView.getView().setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.getView().setAdapter(mAdapter = new BatAdapter(mGoals = new ArrayList<BatModel>() {{
+            add(new Goal("first"));
+            add(new Goal("second"));
+            add(new Goal("third"));
+            add(new Goal("fourth"));
+            add(new Goal("fifth"));
+            add(new Goal("sixth"));
+            add(new Goal("seventh"));
+            add(new Goal("eighth"));
+            add(new Goal("ninth"));
+            add(new Goal("tenth"));
+        }}, this, mAnimator).setOnItemClickListener(this).setOnOutsideClickListener(this));
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new BatCallback(this));
+        itemTouchHelper.attachToRecyclerView(mRecyclerView.getView());
+        mRecyclerView.getView().setItemAnimator(mAnimator);
+        mRecyclerView.setAddItemListener(this);
+
+        findViewById(R.id.root).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRecyclerView.revertAnimation();
+            }
+        });
+    }
+
+    @Override
+    public void add(String string) {
+        mGoals.add(0, new Goal(string));
+        mAdapter.notify(AnimationType.ADD, 0);
+    }
+
+    @Override
+    public void delete(int position) {
+        mGoals.remove(position);
+        mAdapter.notify(AnimationType.REMOVE, position);
+    }
+
+    @Override
+    public void move(int from, int to) {
+        if (from >= 0 && to >= 0) {
+            mAnimator.setPosition(to);
+            BatModel model = mGoals.get(from);
+            mGoals.remove(model);
+            mGoals.add(to, model);
+            mAdapter.notify(AnimationType.MOVE, from, to);
+
+            if (from == 0 || to == 0) {
+                mRecyclerView.getView().scrollToPosition(Math.min(from, to));
+            }
+        }
+    }
+
+    @Override
+    public void onClick(BatModel item, int position) {
+        Toast.makeText(this, item.getText(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onOutsideClicked() {
+        mRecyclerView.revertAnimation();
+    }
+}
+//
+//import android.os.Bundle;
+//import android.support.design.widget.FloatingActionButton;
+//import android.support.design.widget.Snackbar;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.Toolbar;
+//import android.view.View;
+//import android.view.Menu;
+//import android.view.MenuItem;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
